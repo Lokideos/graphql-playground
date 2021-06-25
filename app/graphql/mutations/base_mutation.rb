579a0b2510e81652f5ea3fd8534a2f0546697d57
lokideos <archmage09@gmail.com>
2021-06-25 14:39:31 +0300

    mutations auth refactoring

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,5 +1,10 @@
 module Mutations
   class BaseMutation < GraphQL::Schema::Mutation
+    def check_authentication!
+      return unless context["current_user"]
+
+      raise GraphQL::ExecutionError, "You need to authenticate to perform this action"
+    end
   end
   # class BaseMutation < GraphQL::Schema::RelayClassicMutation
   #   argument_class Types::BaseArgument
